8f17e763c8b443f64f582a7b6b4ed4eefdd66266
Your Name <Littlebirdwarrior>
2023-02-08 15:16:06 +0100

    comit 0.2 -base 14

---
+++ AlgoPHP/Partie1/exo14.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduction PHP - Exo 14</title>
+</head>
+<body>
+    <h1>Introduction PHP - Exo 14</h1>
+    <p> <b>Consigne</b>
+    Calculer l'âge exact d'une personne à partir de sa date de naissance (en années, mois, jours). Affichage (si la date courante est le 21/05/2018 et la date de naissance le 17/01/1985 :<br>
+    Age de la personne : 33 ans 4 mois 4 jours
+    </p>
+
+</body>
+</html>
