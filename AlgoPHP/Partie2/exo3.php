b790cfb42f597aba2588fa4aabce44a7ac4cef18
Camille Dorland <82466002+Littlebirdwarrior@users.noreply.github.com>
2023-02-08 22:56:09 +0100

    exo3

--- AlgoPHP/Partie2/exo3.php
+++ AlgoPHP/Partie2/exo3.php
@@ -9,15 +9,25 @@
 <body>
     <h1>PHP introduction - Partie 2 - Exo 1</h1>
     <p> <b>Consignes : </b><br>
-
+        Afficher un lien hypertexte permettant d’accéder au site d’Elan Formation. Le lien devra s’ouvrir dans un nouvel onglet (_blank).<br>
     </p>
 
-    <!--Mon PHP---->
-    <?php 
-    
+    <p>
+        <b>Voici le lien en HTML: </b><br>
+         <a href="https://elan-formation.eu/" target="_blank" > Lien vers le site Elan Formation</a>
+    </p>
+ 
+    <?PHP 
+    echo "<br><b>Voici le lien en PHP: </b><br>";
+    $link = 'href = "https://elan-formation.eu/"';
+    $target = 'target="_blank"';
+
+    echo "<a ". $link . " " . $target. "> Lien vers le site Elan Formation</a>";
     
     
     ?>
-    
+
+     
+ 
 </body>
 </html>
