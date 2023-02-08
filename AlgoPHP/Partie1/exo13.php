27a8d0a6d6152c5a4d0b158580ca38242e1b0497
Your Name <Littlebirdwarrior>
2023-02-08 15:10:06 +0100

    commit 0.2 - exo 13 putain d'ok

--- AlgoPHP/Partie1/exo13.php
+++ AlgoPHP/Partie1/exo13.php
@@ -7,23 +7,43 @@
     <title>Introduction PHP - Exo 13</title>
 </head>
 <body>
+
+    <h1>Introduction PHP - Exo 13</h1>
+
+    <p><b>Consignes : </b>
+    Calculer la moyenne générale d'un élève dont les notes sont renseignées dans un tableau (pas de coefficient). Elle devra être affichée avec 2 décimales. <fr><br>
+    Affichage :<br>
+    Les notes obtenues par l’élève sont : 10 12 8 19 3 16 11 13 9 <br>
+    Sa moyenne générale est donc de : 11.22 <br>
+    
+    </p>
+
     <?php
-    echo calculerMoyenne([9, 10.5, 11, 18, 12])."<br>";
-    echo calculerMoyenne([19, 10, 3, 8, 19])."<br>";
     
     $eleves = [
-        "cindy" => [12, 9, 19, 17, 12, 13],
-        "pascal" => [8, 9, 12, 10, 17]
+        "Cindy" => [12, 9, 19, 17, 12, 13],
+        "Pascal" => [8, 9, 12, 10, 17, 13],
+        "Thomas" => [8, 10, 12, 10, 17, 16],
     ];
+        
     
     foreach($eleves as $prenom => $notes) {
-        echo "La moyenne de $prenom est : ".calculerMoyenne($notes)."<br>";
+        echo "<b>". $prenom . ": </b> <br>";
+        echo " Les notes obtenues par " . $prenom . " sont :" . displayNotes($notes) . "<br>";
+        echo "Sa moyenne générale est donc de : ".calculerMoyenne($notes)."<br>";
     }
     
+    function displayNotes(array $notes){
+        $displayEachNotes = implode( " ", $notes);
+        return $displayEachNotes;  
+    }
+
+
+
     function calculerMoyenne(array $notes) : float {
         $nbNotes = count($notes);
         $sommeNotes = array_sum($notes);
-        $moyenne = round($sommeNotes / $nbNotes, 2);
+        $moyenne = number_format($sommeNotes / $nbNotes, 2);
     
         return $moyenne;
     }
