2642951f637122e564c28c3c96a266b2bbd67e6f
Your Name <Littlebirdwarrior>
2023-02-09 09:43:25 +0100

    commit 0.3 - exo ok

--- AlgoPHP/Partie2/exo5.php
+++ AlgoPHP/Partie2/exo5.php
@@ -9,13 +9,33 @@
 <body>
     <h1>PHP introduction - Partie 2 - Exo 1</h1>
     <p> <b>Consignes : </b><br>
-
+    Créer une fonction personnalisée permettant de créer un formulaire de champs de texte en 
+    précisant le nom des champs associés.
+    Exemple :
+    $nomsInput = array("Nom","Prénom","Ville");
+    afficherInput($nomsInput);
     </p>
 
     <!--Mon PHP---->
     <?php 
+
+    echo '<br><h2>Affichage : </h2> <br><form>';
+
+    //mes variables
+    $nomsInput = ["Nom","Prénom","Ville"];
+
     
+    function afficherInput($nomsInput){
+
+        foreach($nomsInput as $inputLabel){
+            $inputName = strtolower($inputLabel);
+            echo '<label>'. $inputLabel .': </label><input type="text" name="' . $inputName . '" /><br />';
+        };
+    };
     
+    echo afficherInput($nomsInput);
+
+    echo '<input type="submit" name="submit" value="Envoie!" /></form>';
     
     ?>
     
