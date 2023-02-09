cca33269c37592b60360a92cbe8cb49f85201825
Your Name <Littlebirdwarrior>
2023-02-09 11:53:52 +0100

    commit 0.3 - exo 7 et 8 ok

--- AlgoPHP/Partie2/exo7.php
+++ AlgoPHP/Partie2/exo7.php
@@ -9,13 +9,59 @@
 <body>
     <h1>PHP introduction - Partie 2 - Exo 1</h1>
     <p> <b>Consignes : </b><br>
-
+    Créer une fonction personnalisée permettant de générer des cases à cocher. On pourra préciser dans le tableau associatif si la case est cochée ou non.<br>
+    <b>Exemple :</b><br>
+    genererCheckbox($elements);<br>
+    où $elements est un tableau associatif clé => valeur avec 3 entrées.
     </p>
 
+    <form action="/action_page.php">
+        <input type="checkbox" id="vehicle1" name="vehicle1" value="vehicle1">
+        <label for="vehicle1"> I have a bike</label><br>
+        <input type="submit" value="Submit">
+    </form>
+
+
     <!--Mon PHP---->
-    <?php 
+    <?php
+
+    //Mes variables
+    $elements = [
+        "element-1" => "Primitif",
+        "element-2" => "Berger",
+        "element-3" => "Chasseur",
+        "element-4" => "Terrier",
+        "element-5" => "Molosse",
+
+    ];
     
+    //Ma fonction
+    function genererCheckbox($elements){
+        echo '<h2>Affichage<br>';
+        echo '  <h3>A quel groupe de chien appartient votre compagnon ?</h3>
+                <form action="/action_page.php">';
+
+        foreach ($elements as $element => $elementValue){
+                $specialChar = htmlspecialchars($elementValue);
+                $lowerElement = strtolower($specialChar);
+                echo ' <input 
+                            type="checkbox" 
+                            id="' . $lowerElement . 
+                            ' " name="'. $lowerElement . 
+                            ' " value="' . $lowerElement . '">
+                        <label for="'. $lowerElement . '"> 
+                        I have a ' . $elementValue.
+                        ' </label><br>';
+        }
+
+        
+
+        echo '   <input type="submit" value="Submit">
+                </form>';
+
+    };
     
+    genererCheckbox($elements);
     
     ?>
     
--- AlgoPHP/Partie2/exo8.php
+++ AlgoPHP/Partie2/exo8.php
@@ -5,17 +5,78 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>PHP introduction - Partie 2 - Exo 1</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        figure, img {
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+        }
+
+        .images{
+            display: flex;
+            flex-wrap: wrap;
+        }
+
+        .images figure {
+            width: 250px;
+            height: fit-content;
+            margin: 0 25px 25px  0;
+        }
+
+        .images figure>img {
+            width: 100%;
+        }
+
+        .images>p {
+            color: red
+        }
+    </style>
 </head>
 <body>
     <h1>PHP introduction - Partie 2 - Exo 1</h1>
     <p> <b>Consignes : </b><br>
+    Soit l’URL suivante : http://my.mobirise.com/data/userpic/764.jpg<br>
+    Créer une fonction personnalisée permettant d’afficher l’image N fois à l’écran.<br>
+    <b>Exemple :</b><br>
+    repeterImage($url,4);<br>
 
     </p>
 
     <!--Mon PHP---->
     <?php 
+
+    //mes variables 
+
+    $url = "http://my.mobirise.com/data/userpic/764.jpg";
+    $n = 4;//ici nombre soumis, N comme la consigne le demande.
     
+    //Ma fonction
+    function afficherImage($url, $n){
+    $imageSrc = ' src ="'. $url. '"';
     
+    //Mon encart pour les image
+    echo '<div class="images">';
+    
+    //je vérifie le type de mes variables, en cas de défault, une alerte s'affiche
+    if(gettype($url) == "string" && gettype($n) == "integer" ) {
+        //ma boucle for
+        for ($i = 0; $i < $n; $i++) { 
+            echo '
+            <figure>
+            <img' . $imageSrc. '" alt="Petit chihuahua qui mange une penthoufle"/>
+            </figure>
+            ';
+        }
+    } else { 
+        echo "<p>Alerte : L'url ou le nombre d'affichage demandé sont invalide</p><br>";
+    }
+    
+
+    }
+
+    afficherImage($url, $n);
+
+    echo "</div>";
     
     ?>
     
