7cd6918d7ea8bf8a621a393b56950c2f0b577c36
Littlebirdwarrior <camille.dorland@gmail.com>
2023-02-13 14:59:55 +0100

    commit 0.5 - exercice 15 partie 2 est ok

--- AlgoPHP/Partie2/exo15.php
+++ AlgoPHP/Partie2/exo15.php
@@ -1,29 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>PHP introduction - Partie 2 - Exo 1</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
 <body>
     <h1>PHP introduction - Partie 2 - Exo 1</h1>
     <p> <b>Consignes : </b><br>
-    Ecrire une fonction personnalisée qui affiche une date en français (en toutes lettres) à partir d’une
-    chaîne de caractère représentant une date.<br>
-    Exemple :<br>
-    formaterDateFr("2018-02-23");<br>
-    Affichage<br>
-    vendredi 23 février 2018<br>
+        En utilisant les ressources de la page http://php.net/manual/fr/book.filter.php, vérifier si une
+        adresse e-mail a le bon format.<br>
+        Affichage : <br>
+        L’adresse elan@elan-formation.fr est une adresse e-mail valide <br>
+        L’adresse contact@elan est une adresse e-mail invalide <br>
 
     </p>
 
     <!--Mon PHP---->
-    <?php 
-    
-    
-    
+    <?php
+    $emails = ["elan@elan-formation.fr", " contact@elan"];
+    // Valider l'email
+    foreach ($emails as $email) {
+        echo $email . " : ";
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "L'adresse e-mail est valide <br>";
+        } else {
+            echo "L'adresse e-mail n'est pas valide <br>";
+        }
+
+    }
+
+
+
     ?>
-    
+
 </body>
+
 </html>
